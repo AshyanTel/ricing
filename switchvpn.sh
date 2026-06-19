7b16679ec3418975c2a811e3001038f30f6100ee
ash <ashyantel@proton.me>
2026-06-19 13:12:09 -0400

    Saved before moving

---
+++ switchvpn.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+status="$(protonvpn status)"
+
+if echo "$status" | grep -qi "Disconnected"; then
+    content="$(protonvpn connect)"
+    notify-send "Proton VPN" "$content"
+elif echo "$status" | grep -qi "Connected"; then
+    content="$(protonvpn disconnect)"
+    notify-send "Proton VPN" "$content"
+else
+    notify-send "Proton VPN" "unexpected error"
+fi
