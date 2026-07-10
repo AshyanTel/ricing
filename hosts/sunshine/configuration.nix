cbd384a803795063703aa6c2d4d8bb52e6b02a1f
ash <auxencemirliaz@ashyantel.dev>
2026-07-10 00:59:23 -0400

    WIP

--- hosts/sunshine/configuration.nix
+++ hosts/sunshine/configuration.nix
@@ -67,6 +67,29 @@
       HandlePowerKey = "lock";
       HandlePowerKeyLongPress = "poweroff";
     };
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 50;
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+
+        RUNTIME_PM_ON_AC = "on";
+        RUNTIME_PM_ON_BAT = "auto";
+
+        USB_AUTOSUSPEND = 1;
+      };
+    };
 
   };
 
