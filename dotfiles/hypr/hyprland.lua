aa3ea0e4ceb009353b55e135bc0690de50e76186
ash <auxencemirliaz@ashyantel.dev>
2026-07-12 23:53:46 -0400

    WIP

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -27,7 +27,7 @@ local notification = "mako"
 local idler = "hypridle"
 local lockScreen = "hyprlock"
 local bar = "waybar"
-local wallpaperSetter = "mpvpaper -o "no-audio loop panscan=1.0" '*' ~/dots/dotfiles/hypr/wallpaper.png"
+local wallpaperSetter = "mpvpaper -o \"no-audio loop panscan=1.0\" '*' ~/dots/dotfiles/hypr/wallpaper.png"
 
 -------------------
 ---- AUTOSTART ----
